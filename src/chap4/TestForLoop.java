274ab44ed9e61aa095d8b1e050428074fe52b8f9
subaochen <subaochen@126.com>
2016-03-17 07:53:00 +0800

    增加几个细节的说明

--- src/chap4/TestForLoop.java
+++ src/chap4/TestForLoop.java
@@ -1,5 +1,5 @@
-public class TestForLoop {
-    public static void main(String[] args){
+public class TestForLoop { // |\longremark{类名首字母大写，采用驼峰命名法}|
+    public static void main(String[] args){ // |\longremark{注意缩进和大括号的匹配}|
         for(int i = 0; i < 10; i++) {
             System.out.println(i);
         }
